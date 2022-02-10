8cfd6a676635b656b750f9abcf93d8f8eb9b73e5
Ron Green <11993626+georgettica@users.noreply.github.com>
2022-02-10 13:01:47 +0200

    feat(note): add new endpoint (#9)
    * feat(note): add new endpoint
    
    - change `cluster-missing` endpoint to include basic PD token stuff
      (rewritten 4 times already)
    - errors were renamed/data changed for easier use
    - makefile change to fix a build error
    - moved to use `fmt.Fprint` instead of `w.Write` as it's cleaner and
      doesn't require to cast to a byte array
    - refactored the common errors together
    
    related to OSD-9777
    
    Co-authored-by: Ramon Butter <rbutter@redhat.com>

--- pkg/pagerduty/errors.go
+++ pkg/pagerduty/errors.go
@@ -2,7 +2,7 @@ package pagerduty
 
 import "fmt"
 
-// InvalidTokenErr wraps the pagerduty token invalid error
+// InvalidTokenErr wraps the PagerDuty token invalid error
 type InvalidTokenErr struct {
 	Err error
 }
@@ -19,7 +19,7 @@ func (i InvalidTokenErr) Is(target error) bool {
 	return ok
 }
 
-// InvalidInputParamsErr wraps the pagerduty Invalid parameters error
+// InvalidInputParamsErr wraps the PagerDuty Invalid parameters error
 // TODO: the API also returns any other error in here, if this persists, think on renaming to "ClientMisconfiguration"
 type InvalidInputParamsErr struct {
 	Err error
@@ -43,14 +43,49 @@ type UnknownUpdateIncidentError struct {
 }
 
 // Error prints the wrapped error only
-// this is helpful to make this comply the the error interface
+// this is helpful to make this comply the error interface
 func (i UnknownUpdateIncidentError) Error() string {
-	err := fmt.Errorf("an unknown error was triggered by the caller function: %w", i.Err)
+	err := fmt.Errorf("an unknown error was triggered while updating the incident: %w", i.Err)
 	return err.Error()
 }
 
 // Is ignores the internal error, thus making errors.Is work (as by default it compares the internal objects)
 func (i UnknownUpdateIncidentError) Is(target error) bool {
 	_, ok := target.(UnknownUpdateIncidentError)
+	return ok
+}
+
+// UnknownAddIncidentNoteError wraps the error so it can be handled by the parent function
+type UnknownAddIncidentNoteError struct {
+	Err error
+}
+
+// Error prints the wrapped error only
+// this is helpful to make this comply the error interface
+func (i UnknownAddIncidentNoteError) Error() string {
+	err := fmt.Errorf("an unknown error was triggered while adding a note to the incident: %w", i.Err)
+	return err.Error()
+}
+
+// Is ignores the internal error, thus making errors.Is work (as by default it compares the internal objects)
+func (i UnknownAddIncidentNoteError) Is(target error) bool {
+	_, ok := target.(UnknownAddIncidentNoteError)
+	return ok
+}
+
+// IncidentNotFoundErr wraps the PagerDuty not found error while adding notes to an incident
+type IncidentNotFoundErr struct {
+	Err error
+}
+
+// Error prints the wrapped error and the original one
+func (i IncidentNotFoundErr) Error() string {
+	err := fmt.Errorf("the given incident was not found, can not create note: %w", i.Err)
+	return err.Error()
+}
+
+// Is ignores the internal error, thus making errors.Is work (as by default it compares the internal objects)
+func (i IncidentNotFoundErr) Is(target error) bool {
+	_, ok := target.(IncidentNotFoundErr)
 	return ok
 }
