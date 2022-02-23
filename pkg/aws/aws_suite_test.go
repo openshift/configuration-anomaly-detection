739c2b02d9c4d38bb0647b9b3a6b5413595783ea
Ramon Butter <rbutter@redhat.com>
2022-02-23 15:14:21 +0100

    added list instance functions (#14)

---
+++ pkg/aws/aws_suite_test.go
@@ -0,0 +1,13 @@
+package aws_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestPagerduty(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "AWS Suite")
+}
