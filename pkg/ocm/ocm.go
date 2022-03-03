f477c68aff005da847158b776e616e91b161adc9
Ramon Butter <rbutter@redhat.com>
2022-03-03 16:56:43 +0100

    added ability to fetch support role ARN from ocm client

--- pkg/ocm/ocm.go
+++ pkg/ocm/ocm.go
@@ -21,21 +21,35 @@ type ocmClient struct {
 // New will create a new ocm client by using the path to a config file
 // if no path is provided, it will assume it in the default path
 func New(ocmConfigFile string) (ocmClient, error) {
-
 	client := ocmClient{}
 	cfg, err := newConfigFromFile(ocmConfigFile)
 	if err != nil {
-		return client, fmt.Errorf("failed to load config file: %w", err) // ConfigError{path: ocmConfigFile, err:err}
+		return client, fmt.Errorf("failed to load config file: %w", err)
 	}
 	client.cfg = cfg
 
-	conn, err := client.establishConnection()
+	conn, err := client.cfg.Connection()
 	if err != nil {
-		return client, fmt.Errorf("can't create connection: %w", err) // ConnectionError{client:client, err:err}
+		return client, fmt.Errorf("can't create connection: %w", err)
 	}
 	client.conn = conn
-
 	return client, nil
+}
+
+// GetSupportRoleARN returns the support role ARN that allows the access to the cluster
+func (client ocmClient) GetSupportRoleARN(clusterID string) (string, error) {
+	claim, err := client.GetAWSAccountClaim(clusterID)
+	if err != nil {
+		return "", fmt.Errorf("failed to get account claim: %w", err)
+	}
+	arn := claim.Spec.SupportRoleARN
+	if arn == "" {
+		// if the supportRoleARN is not set, then we won't know which role inside of the customer
+		// AWS account to assume into. This is defined by the customer for STS clusters, and defined
+		// by the aws-account-operator on CCS and OSD accounts
+		return "", fmt.Errorf("AccountClaim is invalid: supportRoleARN is not present in the AccountClaim")
+	}
+	return arn, nil
 }
 
 // GetAWSAccountClaim gets the AWS Account Claim object for a given cluster
@@ -43,22 +57,13 @@ func (client ocmClient) GetAWSAccountClaim(clusterID string) (*awsv1alpha1.Accou
 	ac := &awsv1alpha1.AccountClaim{}
 	acString, err := client.getClusterResource(clusterID, "aws_account_claim")
 	if err != nil {
-		return ac, err
+		return ac, fmt.Errorf("client failed to load GetAWSAccountClaim: %w", err)
 	}
 	err = json.Unmarshal([]byte(acString), ac)
-	return ac, err
-}
-
-// getClusterResource handles caching the live cluster resource endpoint and returns the key
-// you're looking for. This function only caches certain values, as some are not necessary at
-// this time, such as the cluster install logs.
-func (client ocmClient) getClusterResource(clusterID string, resourceKey string) (string, error) {
-
-	response, err := client.conn.ClustersMgmt().V1().Clusters().Cluster(clusterID).Resources().Live().Get().Send()
 	if err != nil {
-		return "", err
+		return ac, fmt.Errorf("failed to unmarshal client response (%s) with error: %w", acString, err)
 	}
-	return response.Body().Resources()[resourceKey], nil
+	return ac, err
 }
 
 // GetClusterDeployment gets the ClusterDeployment object for a given cluster
@@ -66,10 +71,23 @@ func (client ocmClient) GetClusterDeployment(clusterID string) (*hivev1.ClusterD
 	cd := &hivev1.ClusterDeployment{}
 	cdString, err := client.getClusterResource(clusterID, "cluster_deployment")
 	if err != nil {
-		return cd, err
+		return cd, fmt.Errorf("client failed to load ClusterDeployment: %w", err)
 	}
 	err = json.Unmarshal([]byte(cdString), cd)
-	return cd, err
+	if err != nil {
+		return cd, fmt.Errorf("failed to unmarshal client response (%s) with error: %w", cdString, err)
+	}
+	return cd, nil
+}
+
+// getClusterResource allows to load different cluster resources
+func (client ocmClient) getClusterResource(clusterID string, resourceKey string) (string, error) {
+
+	response, err := client.conn.ClustersMgmt().V1().Clusters().Cluster(clusterID).Resources().Live().Get().Send()
+	if err != nil {
+		return "", err
+	}
+	return response.Body().Resources()[resourceKey], nil
 }
 
 // newConfigFromFile loads the configuration file (ocmConfigFile, ~/.ocm.json, /ocm/ocm.json)
@@ -89,41 +107,4 @@ func newConfigFromFile(ocmConfigFile string) (*sdkcfg.Config, error) {
 		return nil, fmt.Errorf("not logged in")
 	}
 	return cfg, err
-}
-
-// establishConnection creates a connection from the client settings
-func (client ocmClient) establishConnection() (*sdk.Connection, error) {
-
-	if client.cfg == (&sdkcfg.Config{}) || client.cfg == nil {
-		return nil, fmt.Errorf("config is empty, aborting")
-	}
-	builder := sdk.NewConnectionBuilder()
-	if client.cfg.TokenURL != "" {
-		builder.TokenURL(client.cfg.TokenURL)
-	}
-	if client.cfg.ClientID != "" || client.cfg.ClientSecret != "" {
-		builder.Client(client.cfg.ClientID, client.cfg.ClientSecret)
-	}
-	if client.cfg.Scopes != nil {
-		builder.Scopes(client.cfg.Scopes...)
-	}
-	if client.cfg.URL != "" {
-		builder.URL(client.cfg.URL)
-	}
-	if client.cfg.User != "" || client.cfg.Password != "" {
-		builder.User(client.cfg.User, client.cfg.Password)
-	}
-	if client.cfg.AccessToken != "" {
-		builder.Tokens(client.cfg.AccessToken)
-	}
-	if client.cfg.RefreshToken != "" {
-		builder.Tokens(client.cfg.RefreshToken)
-	}
-	builder.Insecure(client.cfg.Insecure)
-	// Create the connection:
-	conn, err := builder.Build()
-	if err != nil {
-		return nil, err
-	}
-	return conn, nil
 }
