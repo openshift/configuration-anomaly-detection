90bc8cdf96745338f9c4d15976780a3bd4c79892
Leszek Jakubowski <2552733+Makdaam@users.noreply.github.com>
2025-07-08 11:15:10 +0000

    Adding more cases to the ClusterMonitoringEBB alert investigation.

--- pkg/investigations/clustermonitoringerrorbudgetburn/clustermonitoringerrorbudgetburn_test.go
+++ pkg/investigations/clustermonitoringerrorbudgetburn/clustermonitoringerrorbudgetburn_test.go
@@ -8,19 +8,45 @@ import (
 )
 
 var (
-	statusConditionAvailable                = configv1.ClusterOperatorStatusCondition{Type: "Available", Status: "True"}
-	statusConditionUpgradeable              = configv1.ClusterOperatorStatusCondition{Type: "Upgradeable", Status: "True"}
-	statusConditionUnavailableSymptomsMatch = configv1.ClusterOperatorStatusCondition{Type: "Available", Status: "False", Message: `the User Workload Configuration from "config.yaml" key in the "openshift-user-workload-monitoring/user-workload-monitoring-config" ConfigMap could not be parsed`}
+	statusConditionAvailable                         = configv1.ClusterOperatorStatusCondition{Type: "Available", Status: "True"}
+	statusConditionUpgradeable                       = configv1.ClusterOperatorStatusCondition{Type: "Upgradeable", Status: "True"}
+	statusConditionUnavailableConfigMapSymptomsMatch = configv1.ClusterOperatorStatusCondition{Type: "Available", Status: "False", Message: `the User Workload Configuration from "config.yaml" key in the "openshift-user-workload-monitoring/user-workload-monitoring-config" ConfigMap could not be parsed`}
+	statusConditionUnavailableAMSymptomsMatch        = configv1.ClusterOperatorStatusCondition{Type: "Available", Status: "False", Message: `UpdatingUserWorkloadAlertmanager: waiting for Alertmanager User Workload object changes failed: waiting for Alertmanager openshift-user-workload-monitoring/user-workload: context deadline exceeded: condition Reconciled: status False: reason ReconciliationFailed: provision alertmanager configuration: failed to initialize from secret: address ${SMTP_HOST:-smtp.gmail.com:587}: too many colons in address`}
+	statusConditionUnavailablePMSymptomsMatch        = configv1.ClusterOperatorStatusCondition{Type: "Available", Status: "False", Message: `UpdatingUserWorkloadPrometheus: Prometheus "openshift-user-workload-monitoring/user-workload": NoPodReady: shard 0: pod prometheus-user-workload-0: containers with unready status: [prometheus] shard 0: pod prometheus-user-workload-1: containers with unready status: [prometheus]`}
 )
 
-func TestSymptomMatches(t *testing.T) {
+func TestSymptomMatchesConfigMap(t *testing.T) {
 	monitoringCo := configv1.ClusterOperator{
 		ObjectMeta: v1.ObjectMeta{Name: "monitoring"},
 		Status: configv1.ClusterOperatorStatus{
-			Conditions: []configv1.ClusterOperatorStatusCondition{statusConditionUnavailableSymptomsMatch, statusConditionUpgradeable},
+			Conditions: []configv1.ClusterOperatorStatusCondition{statusConditionUnavailableConfigMapSymptomsMatch, statusConditionUpgradeable},
 		},
 	}
 	if !isUWMConfigInvalid(&monitoringCo) {
+		t.Fatal("expected symptoms to match")
+	}
+}
+
+func TestSymptomMatchesAM(t *testing.T) {
+	monitoringCo := configv1.ClusterOperator{
+		ObjectMeta: v1.ObjectMeta{Name: "monitoring"},
+		Status: configv1.ClusterOperatorStatus{
+			Conditions: []configv1.ClusterOperatorStatusCondition{statusConditionUnavailableAMSymptomsMatch, statusConditionUpgradeable},
+		},
+	}
+	if !isUWMAlertManagerBroken(&monitoringCo) {
+		t.Fatal("expected symptoms to match")
+	}
+}
+
+func TestSymptomMatchesPrometheus(t *testing.T) {
+	monitoringCo := configv1.ClusterOperator{
+		ObjectMeta: v1.ObjectMeta{Name: "monitoring"},
+		Status: configv1.ClusterOperatorStatus{
+			Conditions: []configv1.ClusterOperatorStatusCondition{statusConditionUnavailablePMSymptomsMatch, statusConditionUpgradeable},
+		},
+	}
+	if !isUWMPrometheusBroken(&monitoringCo) {
 		t.Fatal("expected symptoms to match")
 	}
 }
@@ -32,7 +58,9 @@ func TestSymptomNoMatch(t *testing.T) {
 			Conditions: []configv1.ClusterOperatorStatusCondition{statusConditionAvailable, statusConditionUpgradeable},
 		},
 	}
-	if isUWMConfigInvalid(&monitoringCo) {
+	if isUWMConfigInvalid(&monitoringCo) ||
+		isUWMAlertManagerBroken(&monitoringCo) ||
+		isUWMPrometheusBroken(&monitoringCo) {
 		t.Fatal("expected symptoms to not match")
 	}
 }
